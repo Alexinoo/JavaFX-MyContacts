bc88dd5fc01e48774e694fe487b8aa43209464f1
Alex Mwangi <alex@stl-horizon.com>
2024-06-05 07:03:30 +0300

    Initial Setup

--- src/main/java/com/example/javafx_mycontacts/controller/HelloController.java
+++ src/main/java/com/example/javafx_mycontacts/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.javafx_mycontacts;
+package com.example.javafx_mycontacts.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.javafx_mycontacts {
 
     opens com.example.javafx_mycontacts to javafx.fxml;
     exports com.example.javafx_mycontacts;
+    exports com.example.javafx_mycontacts.controller;
+    opens com.example.javafx_mycontacts.controller to javafx.fxml;
 }
