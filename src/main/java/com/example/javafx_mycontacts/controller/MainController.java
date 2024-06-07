1e52cbd5021d5e942b0446cc8cce904928a6e399
Alex Mwangi <alex@stl-horizon.com>
2024-06-07 07:03:56 +0300

    MainController - Open Dialog functionality

---
+++ src/main/java/com/example/javafx_mycontacts/controller/MainController.java
@@ -0,0 +1,44 @@
+package com.example.javafx_mycontacts.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class MainController {
+
+    @FXML
+    private BorderPane mainPane;
+
+    @FXML
+    private void showAddContactDialog() {
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainPane.getScene().getWindow());
+        dialog.setTitle("Add New Contact");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contact-dialog.fxml"));
+
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        }catch (IOException e){
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK){
+
+        }
+
+    }
+}
